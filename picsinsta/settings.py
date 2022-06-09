139b5759012c7b28655075ccbb6ad299a6593806
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-06-09 11:47:02 +0300

    deployment

--- picsinsta/settings.py
+++ picsinsta/settings.py
@@ -1,6 +1,8 @@
 
 import os
 from pathlib import Path
+import django_on_heroku
+from decouple import config
 
 # import cloudinary
 # import cloudinary.uploader
@@ -51,6 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'picsinsta.urls'
@@ -124,10 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -147,3 +155,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CRISPY_TEMPLATE_PACK='bootstrap4'
+
+django_on_heroku.settings(locals())
